0064c14f2201f577b28fc9e757052fc6657f1e59
WeszeRita <weszelovszkirita@gmail.com>
2024-09-24 18:04:49 +0200

    remove setter from custom ngIf

--- directives/src/shared/directives/custom-ng-if.directive.ts
+++ directives/src/shared/directives/custom-ng-if.directive.ts
@@ -1,20 +1,19 @@
-import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, Input, OnChanges, SimpleChanges, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[appCustomNgIf]',
   standalone: true,
 })
-export class CustomNgIfDirective {
-  constructor(private templateRef: TemplateRef<any>, private viewContainer: ViewContainerRef) {
-  }
+export class CustomNgIfDirective implements OnChanges {
+  constructor(private templateRef: TemplateRef<any>, private viewContainer: ViewContainerRef) {}
 
-  @Input()
-  set appCustomNgIf(condition: boolean) {
-    if (condition) {
+  @Input() appCustomNgIf: boolean;
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['appCustomNgIf'].currentValue) {
       this.viewContainer.createEmbeddedView(this.templateRef);
     } else {
       this.viewContainer.clear();
-
     }
   }
 }
