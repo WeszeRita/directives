1a1cbac8c0ad031719ee4ed01b42b8d38209e398
WeszeRita <weszelovszkirita@gmail.com>
2024-10-02 17:07:10 +0200

    custom attribute directive

--- directives/src/app/pages/custom-attribute-directives/usage-highlight-directive/usage-highlight-directive.component.ts
+++ directives/src/app/pages/custom-attribute-directives/usage-highlight-directive/usage-highlight-directive.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { Highlight } from '../../../../shared/directives/highlight.directive';
 import { HighlightWithRenderer2Directive } from '../../../../shared/directives/highlight-with-renderer2.directive';
+import { UppercaseDirective } from '../../../../shared/directives/uppercase.directive';
 
 @Component({
   selector: 'app-usage-highlight-directive',
@@ -8,9 +9,9 @@ import { HighlightWithRenderer2Directive } from '../../../../shared/directives/h
   imports: [
     Highlight,
     HighlightWithRenderer2Directive,
+    UppercaseDirective,
   ],
   templateUrl: './usage-highlight-directive.component.html',
 })
 export class UsageHighlightDirectiveComponent {
-
 }
---
+++ directives/src/shared/directives/uppercase.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[uppercase]',
+  standalone: true,
+})
+export class UppercaseDirective implements OnInit {
+  static elements: HTMLElement[] = [];      // static field will be accessed using dot notation
+
+  @HostListener('click') onClick() {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'textTransform', 'uppercase');
+    UppercaseDirective.elements.forEach((child) => console.log(child.innerText));
+  }
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit() {
+    UppercaseDirective.elements.push(this.elementRef.nativeElement);
+  }
+}
